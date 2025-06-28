7537c7cd2e89064c81a1d19cdecbf3dd158dd919
dariasnegireva <rdsketche@gmail.com>
2025-06-29 00:11:02 +0300

    Menu is ready

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Happy developing ✨')
+const mobileMenuOpener = document.querySelector("#burger-img")
+const mobileMenu = document.querySelector(".wonders-mobile-nav")
+
+mobileMenuOpener.addEventListener("click", (e) => {
+    const isMenuOpened = mobileMenu.classList.contains("wonders-mobile-nav-opened")
+    if (!isMenuOpened) {
+        mobileMenuOpener.src = "styles/img/burger-closed.svg"
+        mobileMenu.classList.add("wonders-mobile-nav-opened")
+    } else {
+        mobileMenuOpener.src = "styles/img/Menu.svg"
+        mobileMenu.classList.remove("wonders-mobile-nav-opened")
+    }
+})
+
