b82ef1670f570f7d91dbd268cf4f3885ee02c3c1
dariasnegireva <rdsketche@gmail.com>
2025-06-30 00:16:49 +0300

    Add README.md and add js logic for counter for person

---
+++ README.md
@@ -0,0 +1,15 @@
+# Site "Wonders"
+
+## What's Inside
+1. Adaptive layout
+2. Burger menu for mobile navigation
+3. Increment with limit 12
+
+## Installation
+1. Clone repository: `git clone https://github.com/dariasnegireva/wonders_daria_snegireva.git`
+2. Open `index.html` in any browser
+
+## Deployment
+Site is deployed using [github pages](https://pages.github.com/).
+
+Site is available by link - [https://dariasnegireva.github.io/wonders_daria_snegireva/](https://dariasnegireva.github.io/wonders_daria_snegireva/)
--- index.js
+++ index.js
@@ -4,11 +4,30 @@ const mobileMenu = document.querySelector(".wonders-mobile-nav")
 mobileMenuOpener.addEventListener("click", (e) => {
     const isMenuOpened = mobileMenu.classList.contains("wonders-mobile-nav-opened")
     if (!isMenuOpened) {
-        mobileMenuOpener.src = "styles/img/burger-closed.svg"
+        mobileMenuOpener.src = "assets/images/burger-closed.svg"
         mobileMenu.classList.add("wonders-mobile-nav-opened")
     } else {
-        mobileMenuOpener.src = "styles/img/Menu.svg"
+        mobileMenuOpener.src = "assets/images/Menu.svg"
         mobileMenu.classList.remove("wonders-mobile-nav-opened")
     }
 })
+
+const personCountInput = document.querySelector("#person-count")
+const personCountPlus = document.querySelector("#person-count-plus")
+const personCountMinus = document.querySelector("#person-count-minus")
+
+personCountPlus.addEventListener("click", (e) => {
+    const currentValue = Number(personCountInput.value)
+    if (currentValue < 12) {
+        personCountInput.value = currentValue + 1
+    }
+})
+
+personCountMinus.addEventListener("click", (e) => {
+    const currentValue = Number(personCountInput.value)
+    if (currentValue > 1) {
+        personCountInput.value = currentValue - 1
+    }
+})
+
 
